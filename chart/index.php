9302b085db59e332440fb8e915f17952c19aedd5
Akshat Mehta <2023pietcaakshat004@poornima.org>
2024-07-02 13:59:51 +0530

    Added a chart

---
+++ chart/index.php
@@ -0,0 +1,179 @@
+<!--
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+-->
+<?php
+
+$servername = "fdb1021.awardspace.net";
+
+// REPLACE with your Database name
+$dbname = "2751591_weatherstation";
+// REPLACE with Database user
+$username = "2751591_weatherstation";
+// REPLACE with Database user password
+$password = "12345678weather";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, value1, value2, value3, reading_time FROM SensorData order by reading_time desc limit 40";
+
+$result = $conn->query($sql);
+
+while ($data = $result->fetch_assoc()){
+    $sensor_data[] = $data;
+}
+
+$readings_time = array();
+foreach ($sensor_data as $data) {
+    if (isset($data['reading_time'])) {
+        $readings_time[] = $data['reading_time'];
+    }
+}
+
+
+// ******* Uncomment to convert readings time array to your timezone ********
+$i = 0;
+foreach ($readings_time as $reading){
+    $readings_time[$i] = date("Y-m-d H:i:s", strtotime("$reading + 5 hours 30 minutes"));
+    $i += 1;
+}
+
+$value1 = json_encode(array_reverse(array_map(function($data) {
+    return isset($data['value1']) ? $data['value1'] : null;
+}, $sensor_data)), JSON_NUMERIC_CHECK);
+
+$value2 = json_encode(array_reverse(array_map(function($data) {
+    return isset($data['value2']) ? $data['value2'] : null;
+}, $sensor_data)), JSON_NUMERIC_CHECK);
+
+$value3 = json_encode(array_reverse(array_map(function($data) {
+    return isset($data['value3']) ? $data['value3'] : null;
+}, $sensor_data)), JSON_NUMERIC_CHECK);
+
+$reading_time = json_encode(array_reverse($readings_time), JSON_NUMERIC_CHECK);
+
+
+/*echo $value1;
+echo $value2;
+echo $value3;
+echo $reading_time;*/
+
+$result->free();
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+  <style>
+    body {
+      min-width: 310px;
+    	max-width: 1280px;
+    	height: 500px;
+      margin: 0 auto;
+    }
+    h2 {
+      font-family: Arial;
+      font-size: 2.5rem;
+      text-align: center;
+    }
+  </style>
+  <body>
+    <h2>Weather Recorder Chart</h2>
+    <div id="chart-temperature" class="container"></div>
+    <div id="chart-humidity" class="container"></div>
+    <div id="chart-pressure" class="container"></div>
+<script>
+
+var value1 = <?php echo $value1; ?>;
+var value2 = <?php echo $value2; ?>;
+var value3 = <?php echo $value3; ?>;
+var reading_time = <?php echo $reading_time; ?>;
+
+var chartT = new Highcharts.Chart({
+  chart:{ renderTo : 'chart-temperature' },
+  title: { text: 'BMP280 Temperature' },
+  series: [{
+    showInLegend: false,
+    data: value1
+  }],
+  plotOptions: {
+    line: { animation: false,
+      dataLabels: { enabled: true }
+    },
+    series: { color: '#059e8a' }
+  },
+  xAxis: { 
+    type: 'datetime',
+    categories: reading_time
+  },
+  yAxis: {
+    title: { text: 'Temperature (Celsius)' }
+    //title: { text: 'Temperature (Fahrenheit)' }
+  },
+  credits: { enabled: false }
+});
+
+var chartH = new Highcharts.Chart({
+  chart:{ renderTo:'chart-humidity' },
+  title: { text: 'BMP280 Pressure' },
+  series: [{
+    showInLegend: false,
+    data: value2
+  }],
+  plotOptions: {
+    line: { animation: false,
+      dataLabels: { enabled: true }
+    }
+  },
+  xAxis: {
+    type: 'datetime',
+    //dateTimeLabelFormats: { second: '%H:%M:%S' },
+    categories: reading_time
+  },
+  yAxis: {
+    title: { text: 'Pressure (hPa)' }
+  },
+  credits: { enabled: false }
+});
+
+
+var chartP = new Highcharts.Chart({
+  chart:{ renderTo:'chart-pressure' },
+  title: { text: 'BMP280 Humidity' },
+  series: [{
+    showInLegend: false,
+    data: value3
+  }],
+  plotOptions: {
+    line: { animation: false,
+      dataLabels: { enabled: true }
+    },
+    series: { color: '#18009c' }
+  },
+  xAxis: {
+    type: 'datetime',
+    categories: reading_time
+  },
+  yAxis: {
+    title: { text: 'Pressure (hPa)' }
+  },
+  credits: { enabled: false }
+});
+
+</script>
+</body>
+</html>
